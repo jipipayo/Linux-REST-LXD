aaad8aaed300b9f6352e2cf1d254145b0f5daf0c
jipipayo <daniel.remeseiro@gmail.com>
2016-09-09 23:22:14 +0200

    first commit, nothing to do here, move along

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Linux::LXD - LXD API REST Client in perl
+
+# SYNOPSIS
+
+    use Linux::LXD;
+
+# DESCRIPTION
+
+Linux::LXD is ...
+
+# LICENSE
+
+Copyright (C) jipipayo.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+jipipayo <jipipayo@cpan.org>
---
+++ lib/Linux/LXD.pm
@@ -0,0 +1,40 @@
+package Linux::LXD;
+use 5.008001;
+use strict;
+use warnings;
+use REST::Client;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Linux::LXD - It's new $module
+
+=head1 SYNOPSIS
+
+    use Linux::LXD;
+
+=head1 DESCRIPTION
+
+Linux::LXD is a API REST client for LXD based on this doc: https://github.com/lxc/lxd/blob/master/doc/rest-api.md
+
+=head1 LICENSE
+
+Copyright (C) jipipayo.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+jipipayo E<lt>jipipayo@cpan.orgE<gt>
+
+=cut
+
